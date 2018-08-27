0e9f6414298401425ab1ee99c541660e876672a8
chris-clifton <cliftonSB@gmail.com>
2018-08-27 11:17:50 -0400

    TTT with Bonus Features Complete

--- lesson6/ttt.rb
+++ lesson6/ttt.rb
@@ -1,6 +1,6 @@
-require 'pry'
-require 'pry-byebug'
+# Tic Tac Toe with Bonus Features
 
+WHO_GOES_FIRST = 'choose' # other options are 'player' and 'computer'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -43,6 +43,27 @@ def joinor(array, delimiter=', ', word='or')
   end
 end
 
+def choose_starting_player
+  prompt 'Please choose starting player:'
+  prompt 'player or computer?'
+  answer = gets.chomp
+  if answer == 'player'
+    player
+  elsif answer == 'computer'
+    computer
+  else
+    prompt 'Sorry, that is not a valid option.'
+  end
+end
+
+def alternate_player(current_player)
+  if current_player == 'player'
+    return 'computer'
+  elsif current_player == 'computer'
+    return 'player'
+  end
+end
+
 def initialize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
@@ -53,6 +74,51 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def take_middle_square(brd)
+  5 if brd[5] == INITIAL_MARKER
+end
+
+def take_random_square(brd)
+  empty_squares(brd).sample
+end
+
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select {|key, value| line.include?(key) && value == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
+def computer_attack_to_win(brd)
+  best_move(brd, COMPUTER_MARKER)
+end
+
+def computer_defend_threat(brd)
+  best_move(brd, PLAYER_MARKER)
+end
+
+def best_move(brd, marker)
+  WINNING_LINES.each do |line|
+   if brd.values_at(*line).count(marker) == 2 &&
+      brd.values_at(*line).count(INITIAL_MARKER) == 1
+      then line.select do |square|
+        return square if empty_squares(brd).include?(square)
+      end
+    end
+    next
+  end
+  nil
+end
+
+def computer_places_piece!(brd)
+  square = computer_attack_to_win(brd)
+  square = computer_defend_threat(brd) unless square
+  square = take_middle_square(brd) unless square
+  square = take_random_square(brd) unless square
+  brd[square] = COMPUTER_MARKER
+end
+
 def player_places_piece!(brd)
   square = ' '
   loop do
@@ -61,14 +127,17 @@ def player_places_piece!(brd)
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
-
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
+def place_piece!(brd, current_player)
+  if current_player == 'player'
+    player_places_piece!(brd)
+  elsif current_player == 'computer'
+    computer_places_piece!(brd)
+  end
 end
+
 
 def board_full?(brd)
   empty_squares(brd).empty?
@@ -102,39 +171,65 @@ end
 
 player_score = 0
 computer_score = 0
+draws = 0
 
-loop do
-  board = initialize_board
-  loop do
+loop do # Main Program loop
+
+  if WHO_GOES_FIRST == 'choose'
+    current_player = nil
+    loop do
+     prompt 'Please choose a starting player:'
+     prompt 'player or computer?'
+     answer = gets.chomp.downcase
+     if answer == 'player'
+       current_player = 'player'
+       break
+     elsif answer == 'computer'
+       current_player = 'computer'
+       break
+     else
+       prompt 'Sorry, that is not a valid option.'
+     end
+   end
+ end
+  
+ loop do
+    board = initialize_board
     display_board(board)
 
-    player_places_piece!(board)
-    display_board(board)
-    break if someone_won?(board) || board_full?(board)
+  #pulled if WHO GOES FIRST From here to test getting outside main game loop
+      
+    loop do
+      display_board(board)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      break if someone_won?(board) || board_full?(board)
+    end
+   
+    display_board(board) 
 
-    computer_places_piece!(board)
-    display_board(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won!"
+    else
+      prompt "It's a tie!"
+    end
+
+    if detect_winner(board) == 'Player'
+      player_score += 1
+    elsif detect_winner(board) == 'Computer'
+      computer_score += 1
+    else
+      draws += 1
+    end
+
+    prompt "Player wins: #{player_score}"
+    prompt "Computer wins: #{computer_score}"
+    prompt "Draws: #{draws}"
+
+    break if player_score >= 5 || computer_score >= 5
+
+    break if play_again?
   end
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  if detect_winner(board) == 'Player'
-    player_score += 1
-  elsif detect_winner(board) == 'Computer'
-    computer_score += 1
-  end
-
-  prompt "Player wins: #{player_score}"
-  prompt "Computer wins: #{computer_score}"
-
-  break if player_score >= 5 || computer_score >= 5
-
-  break if play_again?
 end
 
 prompt "Thanks for playing Tic Tac Toe!"
